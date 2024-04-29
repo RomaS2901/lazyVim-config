a428bc6cb7123e00f11978b161a9629fa138c976
Roman Bondarenko <bondarenko@remed.care>
2024-04-29 15:40:55 +0300

    Venv Selector
    Add new mapping to select cached venv

---
+++ lua/plugins/venv-selector.lua
@@ -0,0 +1,7 @@
+return {
+  "linux-cultist/venv-selector.nvim",
+  keys = {
+    { "<leader>cvc", "<cmd>VenvSelectCached<cr>", desc="Select cached VirtualEnv" },
+    { "<leader>cvs", "<cmd>VenvSelect<cr>", desc="Select VirtualEnv" },
+  },
+}
