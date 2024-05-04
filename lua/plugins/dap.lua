ec41eabaa4e9d9d44e0a55918d58ba052b4d9cae
Roman Bondarenko <romaha2901@gmail.com>
2024-05-04 23:54:53 +0300

    gitsigns, dap
    Add show current line blame
    Add attach for not just my code for DAP

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -10,6 +10,18 @@ return {
     config = function()
       local path = require("mason-registry").get_package("debugpy"):get_install_path()
       require("dap-python").setup(path .. "/venv/bin/python")
+      table.insert(require("dap").configurations.python, {
+        type = "python",
+        request = "attach",
+        name = "Attach Not Just My Code",
+        justMyCode = false,
+        connect = function()
+          local host = vim.fn.input("Host [127.0.0.1]: ")
+          host = host ~= "" and host or "127.0.0.1"
+          local port = tonumber(vim.fn.input("Port [5678]: ")) or 5678
+          return { host = host, port = port }
+        end,
+      })
     end,
   },
 }
